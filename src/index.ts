b505a9f166e173b8d3d058e76145ced81a43fd9f
Allyson Barros <allysonbarrosrn@gmail.com>
2021-05-19 15:50:48 -0300

    Added the Swagger API.

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,10 @@ import { usersRoutes } from "./routes/users.routes";
 const app = express();
 
 app.use(express.json());
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/users", usersRoutes);
 
 export { app };
